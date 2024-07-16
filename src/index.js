55b71a0068946bd040d93c12311158713a3a2046
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-07-16 18:30:30 +0200

    added typewriter plugin and basic form handling

---
+++ src/index.js
@@ -0,0 +1,12 @@
+function generatePoem(event){
+    event.preventDefault();
+
+new Typewriter("#poem",{
+    strings: "Nature's first green is gold,",
+    autoStart: true,
+    cursor:"",
+    delay:1,
+  });}
+
+let poemFormElement=document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit",generatePoem);
